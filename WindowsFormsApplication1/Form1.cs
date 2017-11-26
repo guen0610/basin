6de71ee67b9d16c6aa1dcb45c84b00090b5c5311
guen <guen0610j@gmail.com>
2017-11-26 18:07:47 +0800

    ugugdliin sangaas card_idgaar ni haidag funkts bolson

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -17,6 +17,8 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        public const int FAILED = -10;
+
         SQLiteConnection m_dbConnection;
         static SerialPort mySerial;
         private delegate void displayDelegate(string card, string lname, string name, string phone, string uld);
@@ -29,9 +31,13 @@ namespace WindowsFormsApplication1
             m_dbConnection = new SQLiteConnection(@"Data Source=C:\Users\user\auto.db; Version=3;");
             m_dbConnection.Open();
 
-            string sql = "INSERT INTO users (card_id, lname, name, phone, uld) VALUES ('8jjjk213', 'boldbaatar', 'bold', 89383399, 1)";
-            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
+            if(searchDB("abcd1333") != FAILED)
+            {
+                Console.WriteLine("Success");
+
+            }
+            else
+                Console.WriteLine("Fail");
 
             listViewShow();
 
@@ -51,13 +57,28 @@ namespace WindowsFormsApplication1
 
             
             //System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
-            cardLabel.Text = "wwwwwww";
         }
 
         private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
+            //Serial portnoos irsen utgiig unshina
+            //TODO: Databased baiga esehiig shalgana
+            int ret = searchDB("abcd1333");
+            if(ret == FAILED)
+            {
+                //TODO: iim IDtai hereglegch bhgu bna!!!
+                return;
+            }
             
-            ReadTest("data");   
+            //TODO: Databased oldvol uldegdel ni duussan esehiig shalgana
+            if(ret == 0)
+            {
+                //TODO: Uldegedel duussan bna!!!
+                return;
+            }
+
+            //TODO: Duusagui bol database update hiine
+            //updateDB(ret - 1);  
         }
 
         void WriteTest()
@@ -75,26 +96,29 @@ namespace WindowsFormsApplication1
             }
         }
 
-        void ReadTest(string id)
+        void handleCardPresent(string id)
         {
+
+
+
             // Read sample data from CSV file
-            using (CsvFileReader reader = new CsvFileReader("WriteTest.csv"))
-            {
-                CsvRow row = new CsvRow();
-                while (reader.ReadRow(row))
-                {
-                    if (row[1] != id)
-                        continue;
-                    displayFunc(row[0], row[1], row[2], row[3], row[4]);
-                    //listViewShow();
-                    //Thread demoThread =
-                    //new Thread(new ThreadStart(this.ThreadProcSafe));
+            // using (CsvFileReader reader = new CsvFileReader("WriteTest.csv"))
+            // {
+            //     CsvRow row = new CsvRow();
+            //     while (reader.ReadRow(row))
+            //     {
+            //         if (row[1] != id)
+            //             continue;
+            //         displayFunc(row[0], row[1], row[2], row[3], row[4]);
+            //         //listViewShow();
+            //         //Thread demoThread =
+            //         //new Thread(new ThreadStart(this.ThreadProcSafe));
 
-                    //demoThread.Start();
+            //         //demoThread.Start();
 
-                    //Console.WriteLine();
-                }
-            }
+            //         //Console.WriteLine();
+            //     }
+            // }
         }
 
         void listViewShow()
@@ -112,12 +136,7 @@ namespace WindowsFormsApplication1
                     lvi.SubItems.Add(row[3]);
                     lvi.SubItems.Add(row[4]);
                     listView1.Items.Add(lvi);
-                    //Thread demoThread =
-                    //new Thread(new ThreadStart(this.ThreadProcSafe));
-
-                    //demoThread.Start();
-
-                    //Console.WriteLine();
+                   
                 }
             }
         }
@@ -135,6 +154,27 @@ namespace WindowsFormsApplication1
                 uldLabel.Text = uld;
             }
         }// end fu
+
+        void addUser()
+        {
+            string sql = "INSERT INTO users (card_id, lname, name, phone, uld) VALUES ('8jjjk213', 'boldbaatar', 'bold', 89383399, 1)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        int searchDB(string data)
+        {
+            string sql = String.Format("SELECT card_id, lname, name, phone, uld FROM users WHERE card_id = '{0}'", data);
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("Name: " + reader["name"] + "\tCARD_ID: " + reader["card_id"]);
+                displayFunc(reader["card_id"].ToString(), reader["lname"].ToString(), reader["name"].ToString(), reader["phone"].ToString(), reader["uld"].ToString());
+                return Convert.ToInt32(reader["uld"]);
+            }
+            return FAILED;
+        }
 
         private void label2_Click(object sender, EventArgs e)
         {
